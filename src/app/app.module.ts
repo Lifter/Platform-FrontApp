e941b0b21701e43fd6893ba67fcd2595af9b9b06
Sergey <IgnatenkoSergey@gmail.com>
2024-11-02 20:30:35 +0700

    Добавил меню.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApplicationGridComponent } from './application-grid/application-grid.component';
 import { ApplicationDetailComponent } from './application-detail/application-detail.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApplicationGridComponent,
-    ApplicationDetailComponent
+    ApplicationDetailComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
