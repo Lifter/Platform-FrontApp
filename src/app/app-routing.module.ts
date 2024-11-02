41c6859be2b60af4f128e4c76810acc8186f9fa9
Sergey <IgnatenkoSergey@gmail.com>
2024-11-02 20:47:48 +0700

    Добавил еще одну страницу About чтобы проверить меню.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApplicationGridComponent } from '../app/application-grid/application-grid.component';
 import { ApplicationDetailComponent } from '../app/application-detail/application-detail.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/applications', pathMatch: 'full' },
   { path: 'applications', component: ApplicationGridComponent },
   { path: 'applications/:id', component: ApplicationDetailComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '/applications' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApplicationGridComponent } from './application-grid/application-grid.component';
 import { ApplicationDetailComponent } from './application-detail/application-detail.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApplicationGridComponent,
     ApplicationDetailComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
